34eb52654f183e109864094ef24a8baaca145fd5
abm.ansar@gmail.com <abm.ansar@gmail.com@2276d35e-d7dd-9a90-e99e-9edee6baabbf>
2012-12-18 09:27:08 +0000

    Initial files for this project.

---
+++ trunk/access-controlled.php
@@ -0,0 +1,12 @@
+<?php
+function CheckLogin()
+{
+     session_start();
+     $sessionvar = $this->GetLoginSessionVar();
+     if(empty($_SESSION[$sessionvar]))
+     {
+        return false;
+     }
+     return true;
+}
+?>
---
+++ trunk/logout.php
@@ -0,0 +1,23 @@
+<?PHP
+require_once("./include/membersite_config.php");
+
+$fgmembersite->LogOut();
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>IBM - NextGen</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+</head>
+<body>
+
+<h2>You have logged out</h2>
+<p>
+<a href='login.php'>Login Again</a>
+</p>
+
+</body>
+</html>
